0661ed3f48744f1a76b99887a1223e2e38280371
Alexander Saltykov <alexander-s@yandex-team.ru>
2019-03-14 20:58:02 +0300

    golangflag: gofmt

--- golangflag.go
+++ golangflag.go
@@ -64,11 +64,11 @@ func (v *flagValueWrapper) Type() string {
 func PFlagFromGoFlag(goflag *goflag.Flag) *Flag {
 	// Remember the default value as a string; it won't change.
 	flag := &Flag{
-		Name:  goflag.Name,
-		Usage: goflag.Usage,
-		Value: wrapFlagValue(goflag.Value),
+		Name:     goflag.Name,
+		Usage:    goflag.Usage,
+		Value:    wrapFlagValue(goflag.Value),
 		DefValue: goflag.DefValue,
-		Changed: goflag.DefValue != goflag.Value.String(),
+		Changed:  goflag.DefValue != goflag.Value.String(),
 	}
 	// Ex: if the golang flag was -v, allow both -v and --v to work
 	if len(flag.Name) == 1 {
