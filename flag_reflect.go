96bbf6e9c5f46613243b1b41ca4b6a3bf8c08e74
FengGeSe <18852852150@163.com>
2020-01-19 04:45:19 +0000

    style:add commits

--- flag_reflect.go
+++ flag_reflect.go
@@ -9,6 +9,7 @@ import (
 	"time"
 )
 
+// add flags to FlagSet by struct
 func (f *FlagSet) AddFlags(obj interface{}) error {
 	objT := reflect.TypeOf(obj)
 	if isStructPtr(objT) {
@@ -17,6 +18,7 @@ func (f *FlagSet) AddFlags(obj interface{}) error {
 	return parseFlagsFromTag("", f, objT)
 }
 
+// set struct values from FlagSet
 func (f *FlagSet) SetValues(obj interface{}) error {
 	objT := reflect.TypeOf(obj)
 	objV := reflect.ValueOf(obj)
