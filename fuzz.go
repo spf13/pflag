463f1d927eb62aa2cf317d26f4df32bb61f02d87
AdamKorcz <adam@adalogics.com>
2020-06-03 17:10:19 +0100

    Added fuzzer

---
+++ fuzz.go
@@ -0,0 +1,14 @@
+package pflag
+
+import (
+	"strings"
+)
+
+func Fuzz(data []byte) int {
+	f := NewFlagSet("test", ContinueOnError)
+	err := f.Parse(strings.Split(string(data), " "))
+	if err != nil {
+		return 0
+	}
+	return 1
+}
