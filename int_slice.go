f3827d5ba09132e7e347ff373552bcfeb3563aed
cornfeedhobo <cornfeedhobo@fuzzlabs.org>
2021-03-07 08:21:52 -0600

    Add support for shorthand-only flags
    This commit adds S suffixed methods to support the creation of flags that
    are only exposed to the user in their shorthand form.
    
    This also includes a full standardization of all the wrappers.
    
    Fixes spf13/pflag#256
    Closes spf13/pflag#171

--- int_slice.go
+++ int_slice.go
@@ -112,7 +112,7 @@ func (f *FlagSet) GetIntSlice(name string) ([]int, error) {
 // IntSliceVar defines a intSlice flag with specified name, default value, and usage string.
 // The argument p points to a []int variable in which to store the value of the flag.
 func (f *FlagSet) IntSliceVar(p *[]int, name string, value []int, usage string) {
-	f.VarP(newIntSliceValue(value, p), name, "", usage)
+	f.IntSliceVarP(p, name, "", value, usage)
 }
 
 // IntSliceVarP is like IntSliceVar, but accepts a shorthand letter that can be used after a single dash.
@@ -120,23 +120,31 @@ func (f *FlagSet) IntSliceVarP(p *[]int, name, shorthand string, value []int, us
 	f.VarP(newIntSliceValue(value, p), name, shorthand, usage)
 }
 
+// IntSliceVarS is like IntSliceVar, but accepts a shorthand letter that can be used after a single dash, alone.
+func (f *FlagSet) IntSliceVarS(p *[]int, name, shorthand string, value []int, usage string) {
+	f.VarS(newIntSliceValue(value, p), name, shorthand, usage)
+}
+
 // IntSliceVar defines a int[] flag with specified name, default value, and usage string.
 // The argument p points to a int[] variable in which to store the value of the flag.
 func IntSliceVar(p *[]int, name string, value []int, usage string) {
-	CommandLine.VarP(newIntSliceValue(value, p), name, "", usage)
+	CommandLine.IntSliceVar(p, name, value, usage)
 }
 
 // IntSliceVarP is like IntSliceVar, but accepts a shorthand letter that can be used after a single dash.
 func IntSliceVarP(p *[]int, name, shorthand string, value []int, usage string) {
-	CommandLine.VarP(newIntSliceValue(value, p), name, shorthand, usage)
+	CommandLine.IntSliceVarP(p, name, shorthand, value, usage)
+}
+
+// IntSliceVarS is like IntSliceVar, but accepts a shorthand letter that can be used after a single dash, alone.
+func IntSliceVarS(p *[]int, name, shorthand string, value []int, usage string) {
+	CommandLine.IntSliceVarS(p, name, shorthand, value, usage)
 }
 
 // IntSlice defines a []int flag with specified name, default value, and usage string.
 // The return value is the address of a []int variable that stores the value of the flag.
 func (f *FlagSet) IntSlice(name string, value []int, usage string) *[]int {
-	p := []int{}
-	f.IntSliceVarP(&p, name, "", value, usage)
-	return &p
+	return f.IntSliceP(name, "", value, usage)
 }
 
 // IntSliceP is like IntSlice, but accepts a shorthand letter that can be used after a single dash.
@@ -146,13 +154,25 @@ func (f *FlagSet) IntSliceP(name, shorthand string, value []int, usage string) *
 	return &p
 }
 
+// IntSliceS is like IntSlice, but accepts a shorthand letter that can be used after a single dash, alone.
+func (f *FlagSet) IntSliceS(name, shorthand string, value []int, usage string) *[]int {
+	p := []int{}
+	f.IntSliceVarS(&p, name, shorthand, value, usage)
+	return &p
+}
+
 // IntSlice defines a []int flag with specified name, default value, and usage string.
 // The return value is the address of a []int variable that stores the value of the flag.
 func IntSlice(name string, value []int, usage string) *[]int {
-	return CommandLine.IntSliceP(name, "", value, usage)
+	return CommandLine.IntSlice(name, value, usage)
 }
 
 // IntSliceP is like IntSlice, but accepts a shorthand letter that can be used after a single dash.
 func IntSliceP(name, shorthand string, value []int, usage string) *[]int {
 	return CommandLine.IntSliceP(name, shorthand, value, usage)
+}
+
+// IntSliceS is like IntSlice, but accepts a shorthand letter that can be used after a single dash, alone.
+func IntSliceS(name, shorthand string, value []int, usage string) *[]int {
+	return CommandLine.IntSliceS(name, shorthand, value, usage)
 }
