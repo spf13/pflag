d2a8127570adf182eff87c3c41b618242fb2952c
Dominic Della Valle <ddvpublic@gmail.com>
2022-06-27 13:54:23 -0400

    lint: net.ParseIP doesn't return an error

--- ip_slice.go
+++ ip_slice.go
@@ -72,8 +72,8 @@ func (s *ipSliceValue) String() string {
 	return "[" + out + "]"
 }
 
-func (s *ipSliceValue) fromString(val string) (net.IP, error) {
-	return net.ParseIP(strings.TrimSpace(val)), nil
+func (s *ipSliceValue) fromString(val string) net.IP {
+	return net.ParseIP(strings.TrimSpace(val))
 }
 
 func (s *ipSliceValue) toString(val net.IP) string {
@@ -81,10 +81,7 @@ func (s *ipSliceValue) toString(val net.IP) string {
 }
 
 func (s *ipSliceValue) Append(val string) error {
-	i, err := s.fromString(val)
-	if err != nil {
-		return err
-	}
+	i := s.fromString(val)
 	*s.value = append(*s.value, i)
 	return nil
 }
@@ -92,11 +89,7 @@ func (s *ipSliceValue) Append(val string) error {
 func (s *ipSliceValue) Replace(val []string) error {
 	out := make([]net.IP, len(val))
 	for i, d := range val {
-		var err error
-		out[i], err = s.fromString(d)
-		if err != nil {
-			return err
-		}
+		out[i] = s.fromString(d)
 	}
 	*s.value = out
 	return nil
