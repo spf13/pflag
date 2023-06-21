spf13/pflag

commit 1cd618220ea7ea5792d927ae2bbe39a86c5433e1

Prevent parsing if there are no arguments

flag.go
@@ -406,6 +406,7 @@ func (f *FlagSet) Set(name, value string) error {
 	if !ok {
 		return fmt.Errorf("no such flag -%v", name)
 	}
+
 	err := flag.Value.Set(value)
 	if err != nil {
 		var flagName string
@@ -416,12 +417,15 @@ func (f *FlagSet) Set(name, value string) error {
 		}
 		return fmt.Errorf("invalid argument %q for %q flag: %v", value, flagName, err)
 	}
+
 	if f.actual == nil {
 		f.actual = make(map[NormalizedName]*Flag)
 	}
 	f.actual[normalName] = flag
 	f.orderedActual = append(f.orderedActual, flag)
+
 	flag.Changed = true
+
 	if len(flag.Deprecated) > 0 {
 		fmt.Fprintf(f.out(), "Flag --%s has been deprecated, %s\n", flag.Name, flag.Deprecated)
 	}
@@ -990,6 +994,11 @@ func (f *FlagSet) parseArgs(args []string, fn parseFunc) (err error) {
 // The return value will be ErrHelp if -help was set but not defined.
 func (f *FlagSet) Parse(arguments []string) error {
 	f.parsed = true
+
+	if len(arguments) < 0 {
+		return nil
+	}
+
 	f.args = make([]string, 0, len(arguments))
 
 	set := func(flag *Flag, value string) error {

commit fcfb42bbbbf19c97578e815b7341f7e69ab86287

Parse: fix useless check
Commit 1cd618220ea7ea5 added this check, but the len built-in never
returns negative values.

Change the check to len() == 0.

Fixes: 1cd618220ea7ea5
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

flag.go
@@ -1135,7 +1135,7 @@ func (f *FlagSet) Parse(arguments []string) error {
 	}
 	f.parsed = true
 
-	if len(arguments) < 0 {
+	if len(arguments) == 0 {
 		return nil
 	}
 