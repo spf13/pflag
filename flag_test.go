4aca999b2032843bba3eab005c023550bf6d999e
Frederic BIDON <fredbi@yahoo.com>
2023-10-02 12:16:54 +0200

    ci: replaced travis by gitub action
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- flag_test.go
+++ flag_test.go
@@ -1239,7 +1239,7 @@ func TestPrintDefaults(t *testing.T) {
 	got := buf.String()
 	if got != defaultOutput {
 		fmt.Println("\n" + got)
-		fmt.Println("\n" + defaultOutput)
+		fmt.Printf("\n" + defaultOutput)
 		t.Errorf("got %q want %q\n", got, defaultOutput)
 	}
 }
