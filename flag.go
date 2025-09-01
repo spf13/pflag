spf13/pflag

commit 1bf832c6fec8a35a8c1d61e5fb14f5ce404197ef

Use errors.Is instead of equality check

flag.go
@@ -1173,7 +1173,7 @@ func (f *FlagSet) Parse(arguments []string) error {
 		case ContinueOnError:
 			return err
 		case ExitOnError:
-			if err == ErrHelp {
+			if errors.Is(err, ErrHelp) {
 				os.Exit(0)
 			}
 			fmt.Fprintln(f.Output(), err)
@@ -1202,7 +1202,7 @@ func (f *FlagSet) ParseAll(arguments []string, fn func(flag *Flag, value string)
 		case ContinueOnError:
 			return err
 		case ExitOnError:
-			if err == ErrHelp {
+			if errors.Is(err, ErrHelp) {
 				os.Exit(0)
 			}
 			fmt.Fprintln(f.Output(), err)

commit 44f2a9966892d2b5cfe64adc14152d52d317b4d7

remove uses of errors.Is, which requires go1.13
Commit 1bf832c6fec8a35a8c1d61e5fb14f5ce404197ef introduced the use of
`errors.Is`, which was added in [go1.13], but the `go.mod` was not
updated to reflect this, causing compile failures on go1.12:

    docker run -it --rm -v ./:/pflag -w /pflag golang:1.12 sh -c 'go test -v ./...'
    # github.com/spf13/pflag [github.com/spf13/pflag.test]
    ./flag.go:1190:7: undefined: errors.Is
    ./flag.go:1219:7: undefined: errors.Is
    ./bool_func_test.go:86:28: cannot use stdFSet (type *flag.FlagSet) as type BoolFuncFlagSet in argument to runCase:
        *flag.FlagSet does not implement BoolFuncFlagSet (missing BoolFunc method)
    ...

As the error that is tested will not be wrapped, we can omit the `errors.Is`,
and instead continue doing a straight comparison.

[go1.13]: https://pkg.go.dev/errors@go1.13#Is

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

flag.go
@@ -958,17 +958,17 @@ func (f *FlagSet) usage() {
 // --unknown arg ... (args will be arg ...)
 func stripUnknownFlagValue(args []string) []string {
 	if len(args) == 0 {
-		//--unknown
+		// --unknown
 		return args
 	}
 
 	first := args[0]
 	if len(first) > 0 && first[0] == '-' {
-		//--unknown --next-flag ...
+		// --unknown --next-flag ...
 		return args
 	}
 
-	//--unknown arg ... (args will be arg ...)
+	// --unknown arg ... (args will be arg ...)
 	if len(args) > 1 {
 		return args[1:]
 	}
@@ -1185,7 +1185,7 @@ func (f *FlagSet) Parse(arguments []string) error {
 		case ContinueOnError:
 			return err
 		case ExitOnError:
-			if errors.Is(err, ErrHelp) {
+			if err == ErrHelp {
 				os.Exit(0)
 			}
 			fmt.Fprintln(f.Output(), err)
@@ -1214,7 +1214,7 @@ func (f *FlagSet) ParseAll(arguments []string, fn func(flag *Flag, value string)
 		case ContinueOnError:
 			return err
 		case ExitOnError:
-			if errors.Is(err, ErrHelp) {
+			if err == ErrHelp {
 				os.Exit(0)
 			}
 			fmt.Fprintln(f.Output(), err)