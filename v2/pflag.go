84ddf1643d5bdbcadd2a301e4d038838d27c55bc
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2026-06-26 11:55:42 +0200

    Add placeholder Go package file to satisfy linters

---
+++ v2/pflag.go
@@ -0,0 +1,6 @@
+// Package v2 is the entrypoint for pflag 2.0
+//
+// See https://github.com/spf13/pflag for more info about pflag.
+//
+// v2 is EXTREMELY EXPERIMENTAL AND NOT FOR PRODUCTION USE AT THIS TIME.
+package v2
